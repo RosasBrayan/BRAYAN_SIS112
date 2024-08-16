c8b22370e7788585ddd257c99c5f2e173f1af311
brayan <brayan.rosas@ucb.edu.bo>
2024-08-16 19:19:25 -0400

    EJ2

---
+++ hackerRank/ejercicio2.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    resultado = a + b
+    print(resultado)
+    resultado = a - b
+    print(resultado)
+    resultado = a * b
+    print(resultado)
